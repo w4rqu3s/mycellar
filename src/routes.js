b2d9f8a9b021a38c92fb18482f0a640a553bba2a
Victor Marques <w4rqu3s@gmail.com>
2025-07-19 01:42:46 -0300

    Criação das pages

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,22 @@ const router = createBrowserRouter([
         path: "/", 
         element: null 
     },
+    {
+        path: "/login",
+        element: null
+    },
+    {
+        path: "/signup",
+        element: null
+    },
+    {
+        path: "/drinklist",
+        element: null
+    },
+    {
+        path: "/wannabuy",
+        element: null
+    },
 ])
 
 export default router
